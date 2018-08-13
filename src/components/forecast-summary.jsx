b72df66a010daf9b690abdb006e25dca0ae3022d
Linda <linda.tamakaha@gmail.com>
2018-08-13 23:07:14 +0100

    step 11, test not updated yet

--- src/components/forecast-summary.jsx
+++ src/components/forecast-summary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import WeatherIcon from 'react-icons-weather';
+import Moment from 'moment';
 
 const ForecastSummary = props => (
   <div className="forecast-summary">
@@ -8,13 +9,13 @@ const ForecastSummary = props => (
       <span>{props.temperature}</span>
     </div>
     <div className="forecast-summary__date">
-      <span>{props.date}</span>
+      <span>{moment(props.date).format('ddd Do MMM')}</span>
     </div>
     <div className="forecast-summary__description">
       <span>{props.description}</span>
     </div>
     <div className="forecast-summary__icon">
-      <span>{props.icon}</span>
+      <WeatherIcon name="owm" iconId={props.icon} />
     </div>
   </div>
 );
